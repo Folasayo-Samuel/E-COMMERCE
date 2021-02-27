c73d9155d12d495994830d4d7efc696372d450e7
emreharman <emre.harman@ogr.sakarya.edu.tr>
2021-02-27 23:11:02 +0300

    creating commerce connection and listing products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from "react";
-import Products from "./components/Products/Products";
+import React, { useState, useEffect } from "react";
+import { Products, Navbar } from "./components";
+import { commerce } from "./lib/commerce";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+    setProducts(data);
+  };
+  const fetchCart = async () => {
+    const cart = await commerce.cart.retrieve();
+    setCart(cart);
+  };
+
+  const handleAddCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+    setCart(item.cart);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+  }, []);
   return (
     <div>
-      <Products />
+      <Navbar totalItems={cart.total_items} />
+      <Products products={products} onAddToCart={handleAddCart} />
     </div>
   );
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,25 +1,25 @@
 import React from "react";
 import {Grid} from "@material-ui/core";
 import Product from "./Product/Product";
+import useStyles from "./styles";
 
-const products=[
-    {id:1,name:"Shoes",description:"Running shoes.",price:"$5",image:'https://assets.ajio.com/medias/sys_master/root/h0e/h57/14092954894366/-473Wx593H-460455972-black-MODEL.jpg'},
-    {id:2,name:"Macbook",description:"Apple macbook.",price:"$10",image:'https://cdn03.ciceksepeti.com/cicek/kc3267225-2/XL/codegen-apple-16--macbook-pro-a2141-kilif-kapak-kc3267225-2-b65c056038824f8baa5ac7f0924e203b.jpg'}
-]
 
-const Products=()=>{
+
+const Products=({products,onAddToCart})=>{
+    const classes=useStyles();
     return(
-        <main>
-        <Grid container justify="center" spacing={4}>
-            {
-                products.map((product)=>(
-                    <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product}/>
-                    </Grid>
-                ))
-            }
-        </Grid>
-    </main>
+        <main className={classes.content}>
+            <div className={classes.toolbar}/>
+            <Grid container justify="center" spacing={4}>
+                {
+                    products.map((product)=>(
+                        <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+                            <Product product={product} onAddToCart={onAddToCart}/>
+                        </Grid>
+                    ))
+                }
+            </Grid>
+        </main>
     )
     
 }
